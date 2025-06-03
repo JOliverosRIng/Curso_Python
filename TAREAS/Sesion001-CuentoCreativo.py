6da7d352e41da74a7912ae0146209b212f0b736c
JOliverosRIng <150499716+JOliverosRIng@users.noreply.github.com>
2025-06-03 12:53:44 -0500

    Create a story tile

---
+++ TAREAS/Sesion001-CuentoCreativo.py
@@ -0,0 +1,16 @@
+# Declaraciones de variables a utilizar
+princesa = "Princesa Sofia"
+principe = "Principe Luca"
+sapos = 2
+pajaros = 6
+ratones = 7
+pasteles = 6.7
+castillo = 1.5
+bruja = "Mariam"
+
+# Impresión del cuento
+print("EL CUENTO AL RÉVES")
+print("Este cuento no va hacer muy largo como suelen ser los de 'había una vez' este será conciso.")
+print(f"La {princesa} era hemosa como toda princesa, mimada, malcriada y egocentrica. Nacida para ser rescatada de todo, hasta de sí misma. Mientras que en un país lejano el {principe} jugueteaba con todos los animales, era amable y educado para ser valiente, destinado a rescatar una princesa.")
+print(f"El {principe} y la {princesa} debián estar juntos, y su historia iba a ser decorada por {sapos} sapos, {pajaros} pájaros, {ratones} ratones. En resumen, {sapos+pajaros+ratones} invitados a la boda.")
+print(f"¡Ah! También fue {bruja} La Gran Bruja ¿Y qué pensaste? ¿Que era malvada? Pues no. Era glotona pero delgada, gracias a sus pociones. En la recepción de la boda se comió {pasteles} pasteles ¡Increible!. Sin embargo de un estornudo acabó con {castillo} partes del castillo. Pobre bruja.")
